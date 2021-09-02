7ac940e891610d1396ce9a1ec4859c215610d2fd
DevExpressExampleBot <fakeEmail>
2021-09-02 19:06:37 +0300

    Source auto update [skip ci]

--- VB/MainWindow.xaml.vb
+++ VB/MainWindow.xaml.vb
@@ -1,13 +1,12 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
-Namespace RichEditCustomStylesWpf
+﻿Namespace RichEditCustomStylesWpf
 	Partial Public Class MainWindow
 		Inherits System.Windows.Window
+
 		Public Sub New()
 			InitializeComponent()
 
 			richEditControl1.ApplyTemplate()
-			richEditControl1.ActiveView.ZoomFactor = 0.5f
+			richEditControl1.ActiveView.ZoomFactor = 0.5F
 		End Sub
 	End Class
 End Namespace
